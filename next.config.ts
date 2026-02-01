4edc6e58d4b0d69d78956d9a2cc77cc44239b7bc
DavinMetoti <davinmetotii@gmail.com>
2026-02-02 02:20:29 +0700

    Update remote image hostname to cms.medikost.id

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cms.mediko.id',
+        hostname: 'cms.medikost.id',
       },
     ],
   },
